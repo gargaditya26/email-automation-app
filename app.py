4491b77eda995c8d82677e9b6016997e987e7ae3
Aditya Garg <adityagarg2603@gmail.com>
2026-03-18 23:50:53 +0530

    Updated

--- app.py
+++ app.py
@@ -165,6 +165,13 @@ def send():
 def home():
     return render_template("index.html")
 
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
